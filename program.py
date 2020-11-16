145af9d83a2ce6e0f8aea537b0f6fa0bb6032128
Natikkk <s770@mail.ru>
2020-11-16 17:39:19 +0300

    Commit 3

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program')
     print('И я изменяю это каждый день')
     print('Again')
+    print('UFO came and added this line')
 
 
 if __name__ == '__main__':
